6f9573c4cc9235c621482a8a8d629ec50e3b1d87
G3ru1a <egidiufarcas@maze.ws>
2021-08-12 16:09:22 +0100

    Post RESTAPI no Auth

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index(){
+        return Post::all();
+    }
+
+    public function store(Request $request){
+        try {
+            $post = new Post();
+            $post->title = $request->title;
+            $post->body = $request->body;
+            if($post->save()){
+                return response()->json(['status' => 'success', 'message' => 'Post Created Successfully.']);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+        }
+    }
+
+    public function update(Request $request, $id){
+        try {
+            $post = Post::findOrFail($id);
+            $post->title = $request->title;
+            $post->body = $request->body;
+            if($post->save()){
+                return response()->json(['status' => 'success', 'message' => 'Post Updated Successfully.']);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+        }
+    }
+
+    public function destroy(Request $request, $id){
+        try {
+            $post = Post::findOrFail($id);
+            if($post->delete()){
+                return response()->json(['status' => 'success', 'message' => 'Post Deleted Successfully.']);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+        }
+    }
+}
